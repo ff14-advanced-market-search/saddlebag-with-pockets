4684f002034e0343bfe73a214e6db9d301d7fc16
Zac Bruce <zac@vyne.co.nz>
2022-08-11 16:29:49 +1200

    Needs more tests

--- app/utils/locations/locations.test.ts
+++ app/utils/locations/locations.test.ts
@@ -1,7 +1,42 @@
-import {DataCentersOfRegion} from "./index";
+import {DataCentersOfRegion, WorldsOfDataCenter} from "./index";
+import {DataCenterNotFoundException, RegionNotFoundException} from "~/utils/locations/Errors";
 
 test('DataCentersOfRegion returns a data center array', () => {
     expect(DataCentersOfRegion('OCE')).toEqual([{
         name: 'Materia',
     }]);
+})
+
+test('DataCentersOfRegion throws RegionNotFoundException', () => {
+    expect(() => {
+        DataCentersOfRegion('NOT-REAL-REGION')
+    }).toThrow(RegionNotFoundException)
+})
+
+test('WorldsOfDataCenter returns a world array', () => {
+    expect(WorldsOfDataCenter('Mana')).toEqual([{
+        "name": "Anima"
+    }, {
+        "name": "Asura"
+    }, {
+        "name": "Chocobo"
+    }, {
+        "name": "Hades"
+    }, {
+        "name": "Ixion"
+    }, {
+        "name": "Masamune"
+    }, {
+        "name": "Pandaemonium"
+    }, {
+        "name": "Titan"
+    }]);
+})
+
+
+
+test('WorldsOfDataCenter throws DataCenterNotFoundException', () => {
+    expect(() => {
+        WorldsOfDataCenter('Something about hot dogs')
+    }).toThrow(DataCenterNotFoundException)
 })
