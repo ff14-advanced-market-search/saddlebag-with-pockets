1a336b916e55843ae1dc6c3fc2ac8dad92597df1
Zac Bruce <zac@vyne.co.nz>
2022-08-26 15:35:52 +1200

    remove some fluff

--- app/routes/queries/full-scan.tsx
+++ app/routes/queries/full-scan.tsx
@@ -15,15 +15,15 @@ const FullScan = () => {
                                             Scan Hours
                                         </label>
                                         <div className={`mt-1 flex rounded-md shadow-sm`}>
-                                        <input
-                                            type="number"
-                                            id="scan-hours"
-                                            defaultValue={24}
-                                            name="scan-hours"
-                                            className="flex-1 min-w-0 block w-full px-3 py-2 rounded-none rounded-l-md focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        />
-                                        <span
-                                            className={`inline-flex items-center px-3 rounded-r-md border border-l-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm`}>
+                                            <input
+                                                type="number"
+                                                id="scan-hours"
+                                                defaultValue={24}
+                                                name="scan-hours"
+                                                className="flex-1 min-w-0 block w-full px-3 py-2 rounded-none rounded-l-md focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            />
+                                            <span
+                                                className={`inline-flex items-center px-3 rounded-r-md border border-l-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm`}>
                                                 hours
                                             </span>
                                         </div>
@@ -73,98 +73,9 @@ const FullScan = () => {
                                         </p>
                                     </div>
 
-                                    <div className="col-span-6 sm:col-span-2">
-                                        <label htmlFor="email-address"
-                                               className="block text-sm font-medium text-gray-700">
-                                            Email address
-                                        </label>
-                                        <input
-                                            type="text"
-                                            name="email-address"
-                                            id="email-address"
-                                            autoComplete="email"
-                                            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        />
-                                    </div>
 
-                                    <div className="col-span-6 sm:col-span-3">
-                                        <label htmlFor="country" className="block text-sm font-medium text-gray-700">
-                                            Country
-                                        </label>
-                                        <select
-                                            id="country"
-                                            name="country"
-                                            autoComplete="country-name"
-                                            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                                        >
-                                            <option>United States</option>
-                                            <option>Canada</option>
-                                            <option>Mexico</option>
-                                        </select>
-                                    </div>
-
-                                    <div className="col-span-6">
-                                        <label htmlFor="street-address"
-                                               className="block text-sm font-medium text-gray-700">
-                                            Street address
-                                        </label>
-                                        <input
-                                            type="text"
-                                            name="street-address"
-                                            id="street-address"
-                                            autoComplete="street-address"
-                                            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        />
-                                    </div>
-
-                                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                                        <label htmlFor="city" className="block text-sm font-medium text-gray-700">
-                                            City
-                                        </label>
-                                        <input
-                                            type="text"
-                                            name="city"
-                                            id="city"
-                                            autoComplete="address-level2"
-                                            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        />
-                                    </div>
-
-                                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                                        <label htmlFor="region" className="block text-sm font-medium text-gray-700">
-                                            State / Province
-                                        </label>
-                                        <input
-                                            type="text"
-                                            name="region"
-                                            id="region"
-                                            autoComplete="address-level1"
-                                            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        />
-                                    </div>
-
-                                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                                        <label htmlFor="postal-code"
-                                               className="block text-sm font-medium text-gray-700">
-                                            ZIP / Postal code
-                                        </label>
-                                        <input
-                                            type="text"
-                                            name="postal-code"
-                                            id="postal-code"
-                                            autoComplete="postal-code"
-                                            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        />
-                                    </div>
                                 </div>
-                            </div>
-                            <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                <button
-                                    type="submit"
-                                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                                >
-                                    Save
-                                </button>
+
                             </div>
                         </div>
                     </Form>
