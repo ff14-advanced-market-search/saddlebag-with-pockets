fd36841690773b9c12b162212f9c22d3490141bd
Zac Bruce <zac@vyne.co.nz>
2022-08-26 12:57:41 +1200

    Nicer words

--- app/routes/options/index.tsx
+++ app/routes/options/index.tsx
@@ -134,7 +134,7 @@ export default function () {
                                 <div className="md:grid md:grid-cols-3 md:gap-6">
                                     <div className="md:col-span-1">
                                         <div className="px-4 sm:px-0">
-                                            <h3 className="text-lg font-medium leading-6 text-gray-900">View</h3>
+                                            <h3 className="text-lg font-medium leading-6 text-gray-900">Theme</h3>
                                             <p className="mt-1 text-sm text-gray-600">
                                                 Needs more sparkles.. ✨✨✨✨
                                             </p>
