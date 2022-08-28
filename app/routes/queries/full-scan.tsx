ee49d7b3109089aeffaf92e7e868a7894d76257e
Zac Bruce <zac@vyne.co.nz>
2022-08-29 11:58:40 +1200

    Update some tooltips & labels

--- app/routes/queries/full-scan.tsx
+++ app/routes/queries/full-scan.tsx
@@ -163,7 +163,7 @@ const FullScan = () => {
 
                                     <div className="col-span-6 sm:col-span-2">
                                         <fieldset className="space-y-5">
-                                            <legend className="sr-only">HQ</legend>
+                                            <legend className="sr-only">Force HQ only</legend>
                                             <div className="relative flex items-start">
                                                 <div className="flex items-center h-5">
                                                     <input
@@ -204,7 +204,7 @@ const FullScan = () => {
                                                         Region Wide Search
                                                     </label>
                                                     <p className="mt-2 text-sm text-gray-500">
-                                                        Search all servers in all DataCenters in your region or just search the local DataCenter.
+                                                        Search all servers in all DataCenters in your region.
                                                     </p>
                                                 </div>
                                             </div>
@@ -213,7 +213,7 @@ const FullScan = () => {
 
                                     <div className="col-span-6 sm:col-span-2">
                                         <fieldset className="space-y-5">
-                                            <legend className="sr-only">HQ</legend>
+                                            <legend className="sr-only">Include Vendor Prices</legend>
                                             <div className="relative flex items-start">
                                                 <div className="flex items-center h-5">
                                                     <input
@@ -229,7 +229,7 @@ const FullScan = () => {
                                                         Include Vendor Prices
                                                     </label>
                                                     <p className="mt-2 text-sm text-gray-500">
-                                                        HQ only? Only search for hq prices
+                                                        Compare market prices vs vendor prices on NQ items that can be purchased from vendors.
                                                     </p>
                                                 </div>
                                             </div>
@@ -238,23 +238,23 @@ const FullScan = () => {
 
                                     <div className="col-span-6 sm:col-span-2">
                                         <fieldset className="space-y-5">
-                                            <legend className="sr-only">HQ</legend>
+                                            <legend className="sr-only">Include Out of Stock</legend>
                                             <div className="relative flex items-start">
                                                 <div className="flex items-center h-5">
                                                     <input
-                                                        id="hq_only"
+                                                        id="out_of_stock"
                                                         aria-describedby="comments-description"
-                                                        name="hq_only"
+                                                        name="out_of_stock"
                                                         type="checkbox"
                                                         className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
                                                     />
                                                 </div>
                                                 <div className="ml-3 text-sm">
-                                                    <label htmlFor="hq_only" className="font-medium text-gray-700">
-                                                        Enable HQ only
+                                                    <label htmlFor="out_of_stock" className="font-medium text-gray-700">
+                                                        Include Out of Stock
                                                     </label>
                                                     <p className="mt-2 text-sm text-gray-500">
-                                                        HQ only? Only search for hq prices
+                                                        Include out of stock items from the list (they will show up as having 100% profit margins and 1 bil gil profit).
                                                     </p>
                                                 </div>
                                             </div>
