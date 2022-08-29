9eee1e1e0ab7c154c86904a592958ecd9d629598
Zac Bruce <zac@vyne.co.nz>
2022-08-29 16:51:05 +1200

    Uuuuse the remix

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+module.exports = {serverBuildTarget: "cloudflare-pages",
+  server: "./server.js",
+  devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
