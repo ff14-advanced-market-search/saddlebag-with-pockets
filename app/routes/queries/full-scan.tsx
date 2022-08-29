3dd4923306ae3d61280cca15147d843de80fad0c
Zac Bruce <zac@vyne.co.nz>
2022-08-29 19:54:46 +1200

    Darken bg by a few %

--- app/routes/queries/full-scan.tsx
+++ app/routes/queries/full-scan.tsx
@@ -302,7 +302,7 @@ const FullScan = () => {
                         <button
                             type="submit"
                             onClick={onSubmit}
-                            className={classNames(isRunning ? 'bg-gray-500' : 'bg-blue-500', "cursor-pointer ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500")}
+                            className={classNames(isRunning ? 'bg-gray-500' : 'bg-blue-600', "cursor-pointer ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500")}
                         >
                             {isRunning && <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
