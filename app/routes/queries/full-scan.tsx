17e5ffb99913dbc5edb58b826ff290b02bd37e2e
Zac Bruce <zac@vyne.co.nz>
2022-08-29 17:01:58 +1200

    Change form minimum stack size

--- app/routes/queries/full-scan.tsx
+++ app/routes/queries/full-scan.tsx
@@ -115,7 +115,7 @@ const FullScan = () => {
                                             <input
                                                 type="number"
                                                 name="minimum_stack_size"
-                                                defaultValue={10}
+                                                defaultValue={1}
                                                 id="minimum_stack_size"
                                                 className="flex-1 min-w-0 block w-full px-3 py-2 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                             />
