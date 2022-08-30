74b28491f797e3a95306cbf0f9d764f142dfe1a6
Zac Bruce <zac@vyne.co.nz>
2022-08-30 12:16:53 +1200

    ts ignore irrelevant type

--- app/routes/queries/full-scan.tsx
+++ app/routes/queries/full-scan.tsx
@@ -292,6 +292,7 @@ const FullScan = () => {
                     <div className="flex justify-end">
                         <button
                             type="submit"
+                            // @ts-ignore
                             onClick={onSubmit}
                             className={classNames(isRunning ? 'bg-gray-500' : 'bg-blue-600', "cursor-pointer ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500")}
                         >
