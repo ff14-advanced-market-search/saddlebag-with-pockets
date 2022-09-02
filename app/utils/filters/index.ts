fbfe7a9eb9c36f968c03937bdb23fd1a4a2aee57
Zac Bruce <zac@vyne.co.nz>
2022-09-02 13:42:59 +1200

    Add some IDentifiers

--- app/utils/filters/index.ts
+++ app/utils/filters/index.ts
@@ -1,26 +1,56 @@
-import {arms} from "~/utils/filters/arms";
-import {tools} from "~/utils/filters/tools";
-import {ReactNode} from "react";
-import {armor} from "~/utils/filters/armor";
-import {accessories} from "~/utils/filters/accessories";
-import {materials} from "~/utils/filters/materials";
-import {medicinesMeals} from "~/utils/filters/medicines-meals";
-import {other} from "~/utils/filters/other";
+import {arms}           from "~/utils/filters/arms"
+import {tools}          from "~/utils/filters/tools"
+import {ReactNode}      from "react"
+import {armor}          from "~/utils/filters/armor"
+import {accessories}    from "~/utils/filters/accessories"
+import {materials}      from "~/utils/filters/materials"
+import {medicinesMeals} from "~/utils/filters/medicines-meals"
+import {other}          from "~/utils/filters/other"
 
 export type FilterFormat = {
-    name: string, abbreviation?: string, icon: ReactNode,
+    name: string, abbreviation?: string, icon: ReactNode, id: number,
 }
 type FiltersList = {
-    name: string, data: FilterFormat[]
+    name: string, data: FilterFormat[], id: number
 }[]
 
-const filters: FiltersList = [{
-    name: "Arms", data: arms
-}, {
-    name: "Tools", data: tools
-}, {name: "Armor", data: armor}, {name: "Accessories", data: accessories}, {
-    name: "Medicines & Meals", data: medicinesMeals
-}, {name: "Materials", data: materials}, {name: "Other", data: other}]
+const filters: FiltersList = [
+    {
+        name: "Arms",
+        data: arms,
+        id:   1,
+    },
+    {
+        name: "Tools",
+        data: tools,
+        id:   2,
+    },
+    {
+        name: "Armor",
+        data: armor,
+        id:   3
+    },
+    {
+        name: "Accessories",
+        data: accessories,
+        id:   4
+    },
+    {
+        name: "Medicines & Meals",
+        data: medicinesMeals,
+        id:   5
+    },
+    {
+        name: "Materials",
+        data: materials,
+        id:   6
+    },
+    {
+        name: "Other",
+        data: other,
+        id:   7
+    }
+]
 
 
 export default filters
