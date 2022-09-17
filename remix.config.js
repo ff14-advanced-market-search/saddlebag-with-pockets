6836cedae5ca286a5382595523dde90b09eedf7d
Tuft <zac@tuft.co.nz>
2022-09-17 17:58:40 +1200

    Test esbuild config inb4 explosions

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,19 @@
+const { withEsbuildOverride } = require('remix-esbuild-override')
+
+withEsbuildOverride((option, { isServer, isDev }) => {
+  // update the option
+  option.logOverride = { 'this-is-undefined-in-esm': 'silent' }
+
+  return option
+})
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {serverBuildTarget: "cloudflare-pages",
-  server: "./server.js",
+module.exports = {
+  serverBuildTarget: 'cloudflare-pages',
+  server: './server.js',
   devServerBroadcastDelay: 1000,
-  ignoredRouteFiles: ["**/.*"],
+  ignoredRouteFiles: ['**/.*']
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-};
+}
