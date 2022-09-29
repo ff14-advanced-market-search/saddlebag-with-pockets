cd4de60aafbf1db4cd548b5daf1c93e6db61f021
cohenaj194 <cohenaj194@gmail.com>
2022-09-29 01:17:17 -0400

    update ad note

--- app/routes/why/advertisements.tsx
+++ app/routes/why/advertisements.tsx
@@ -4,7 +4,9 @@ const Advertisements = () => {
       <main className="flex-1">
         <h2 className={`text-2xl`}>Why do we have advertisements?</h2>
         <p className={`text-prose`}>
-          Basically, this is an incredibly expensive operation to run.
+          Basically, this is an incredibly expensive operation to run. 
+          These ads help keep the servers and databases running. 
+          If you want to help us out consider supporting us on Patreon or Ko-fi.
         </p>
       </main>
     </div>
