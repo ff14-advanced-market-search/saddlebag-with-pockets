a3b00760aebab0f743a0e716e0e1c3ad08cad76e
cohenaj194 <cohenaj194@gmail.com>
2022-10-12 14:05:29 -0400

    try with median values

--- app/routes/queries/listings/index.tsx
+++ app/routes/queries/listings/index.tsx
@@ -131,12 +131,34 @@ const Index = () => {
                 }
               />
             )}
+            {'listing_price_diff' in results && (
+              <Differences
+                diffTitle="Median Price Difference"
+                diffAmount={results.listing_price_diff.median_price_diff}
+                className={
+                  results.listing_price_diff.median_price_diff >= 10000
+                    ? 'bg-red-100 font-semibold text-red-800'
+                    : 'bg-green-100 font-semibold text-green-800'
+                }
+              />
+            )}
             {'listing_time_diff' in results && (
               <Differences
                 diffTitle="Avg Time Difference"
                 diffAmount={`${results.listing_time_diff.avg_time_diff} minutes`}
                 className={
                   results.listing_time_diff.avg_time_diff >= 30
+                    ? 'bg-green-100 font-semibold text-green-800'
+                    : 'bg-red-100 font-semibold text-red-800'
+                }
+              />
+            )}
+            {'listing_time_diff' in results && (
+              <Differences
+                diffTitle="Median Time Difference"
+                diffAmount={`${results.listing_time_diff.median_time_diff} minutes`}
+                className={
+                  results.listing_time_diff.median_time_diff >= 30
                     ? 'bg-green-100 font-semibold text-green-800'
                     : 'bg-red-100 font-semibold text-red-800'
                 }
