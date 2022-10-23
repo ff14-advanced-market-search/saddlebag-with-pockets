51cc27ea77b16e3f78e45a050aa169a9231d6335
Ben Webb <b-webb@outlook.com>
2022-10-23 11:41:18 +0100

    Update types on conversion function

--- app/routes/queries/full-scan/convertResponseToTableRows.ts
+++ app/routes/queries/full-scan/convertResponseToTableRows.ts
@@ -1,7 +1,7 @@
 import type { ResponseType } from '~/requests/FullScan'
 
 export const convertResponseToTableRows = (
-  data: ResponseType
+  data: Record<string, ResponseType>
 ): Array<ResponseType> => {
   return Object.entries(data).map((entry: [string, any]) => {
     return { id: parseInt(entry[0]), ...entry[1] }
