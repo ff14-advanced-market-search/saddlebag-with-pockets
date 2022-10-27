40775180b61081e058e9ddb6c6ea19990fa1e26b
Ben Webb <b-webb@outlook.com>
2022-10-27 22:13:14 +0100

    Update styling on components

--- app/components/Common/PageWrapper.tsx
+++ app/components/Common/PageWrapper.tsx
@@ -1,5 +1,5 @@
 export const PageWrapper = ({ children }: { children: JSX.Element }) => (
   <main className="flex-1 dark:bg-slate-800">
-    <div className="py-3 px-4">{children}</div>
+    <div className="py-1 px-1 sm:py-3 sm:px-4">{children}</div>
   </main>
 )
