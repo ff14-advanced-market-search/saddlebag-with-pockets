dd8f771c26fc0f94b31550105efa4dde2cdd1ab0
cohenaj194 <cohenaj194@gmail.com>
2022-10-28 09:40:20 -0400

    fix order and columns

--- app/routes/wow/full-scan/Results.tsx
+++ app/routes/wow/full-scan/Results.tsx
@@ -8,42 +8,43 @@ import type { ColumnList } from './ReusableTable'
 
 export const Results = ({ data }: { data: WoWScanResponse }) => {
   const oosColumnList: Array<ColumnList<WoWOutOfStock>> = [
-    { columnId: 'itemID', header: 'Item ID' },
     { columnId: 'name', header: 'Item Name' },
+    { columnId: 'itemID', header: 'Item ID' },
     { columnId: 'price', header: 'Price' },
     { columnId: 'historicPrice', header: 'Historic Price' },
     { columnId: 'salesPerDay', header: 'Sales Per Day' }
   ]
 
   const profitableItemsColumnList: Array<ColumnList<WoWProfitableItems>> = [
-    { columnId: 'itemID', header: 'Item ID' },
     { columnId: 'name', header: 'Item Name' },
-    { columnId: 'home_price', header: 'Home World Price' },
-    { columnId: 'new_price', header: 'New World Price' },
-    { columnId: 'salesPerDay', header: 'Sales Per Day' },
+    { columnId: 'itemID', header: 'Item ID' },
+    { columnId: 'profit', header: 'Profit Per Sale' },
+    { columnId: 'historicPrice', header: 'Historic Price' },
     {
       columnId: 'roi',
       header: 'ROI Percentage',
       newComponent: (value) => <p>{`${value}%`}</p>
     },
+    { columnId: 'home_price', header: 'Home World Price' },
+    { columnId: 'new_price', header: 'New World Price' },
     { columnId: 'salesPerDay', header: 'Sales Per Day' }
   ]
 
   return (
     <>
-      <ReusableTable<WoWOutOfStock>
-        data={data.out_of_stock}
-        columnList={oosColumnList}
-        sortingOrder={[{ id: 'price', desc: true }]}
-        title="Out of Stock Items"
-        description="This shows some out of stock stuff... Sell for mega profits $$$ "
-      />
       <ReusableTable<WoWProfitableItems>
         data={data.profitable_items}
         columnList={profitableItemsColumnList}
         sortingOrder={[{ id: 'new_price', desc: true }]}
         title="Profitable Items"
-        description="This shows some profitable stuff... Sell for mega profits $$$"
+        description="This shows items you can buy on your home server and sell on your new server for a profit!"
+      />
+      <ReusableTable<WoWOutOfStock>
+        data={data.out_of_stock}
+        columnList={oosColumnList}
+        sortingOrder={[{ id: 'price', desc: true }]}
+        title="Out of Stock Items"
+        description="This shows items you can buy on your home server that are not listed on the new server.  You can sell them for any price you want!"
       />
     </>
   )
