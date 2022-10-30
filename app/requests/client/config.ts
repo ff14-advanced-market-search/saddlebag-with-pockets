bfb75603d8fbb424de9f0d94a98cd93ac46db576
Ben Webb <b-webb@outlook.com>
2022-10-30 22:45:19 +0000

    Update server to point at temp endpoint

--- app/requests/client/config.ts
+++ app/requests/client/config.ts
@@ -1,5 +1,6 @@
-const defaultAddress = `http://api.saddlebagexchange.com`
-export const address = defaultAddress
+export const defaultAddress = `http://api.saddlebagexchange.com`
+// export const address = defaultAddress
+export const address = 'http://24.199.70.65'
 // @todo update via env var
 // export const address =
 //   process.env.NODE_ENV === 'development'
