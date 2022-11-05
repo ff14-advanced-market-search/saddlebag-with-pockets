d79efa61834dd28cda8e9c015a1c18eb0d3f4d5a
Zac Bruce <zac@vyne.co.nz>
2022-11-05 22:37:11 +1300

    Attempt encrypting session cookie. How 2 cloudflare?

--- app/sessions/index.ts
+++ app/sessions/index.ts
@@ -1,37 +1,47 @@
 import { createCookieSessionStorage } from '@remix-run/cloudflare'
-import { WorldsArray } from '~/utils/locations/Worlds'
-import { DataCenterArray } from '~/utils/locations/DataCenters'
+import { WorldsArray }                from '~/utils/locations/Worlds'
+import { DataCenterArray }            from '~/utils/locations/DataCenters'
 
-const { getSession, commitSession, destroySession } =
-  createCookieSessionStorage({
-    cookie: {
-      name: '__session',
-      secure: process.env.NODE_ENV === 'production',
-      httpOnly: true,
-      sameSite: 'lax',
-      path: '/'
-    }
-  })
+// const secret = SESSION_COOKIE_SECRET
+const {
+        getSession,
+        commitSession,
+        destroySession
+      } = createCookieSessionStorage({
+  cookie: {
+    name:     '__session',
+    secure:   process.env.NODE_ENV === 'production',
+    httpOnly: true,
+    sameSite: 'lax',
+    secrets:  [process.env.SESSION_COOKIE_SECRET],
+    path:     '/'
+  }
+})
 
 async function getUserSessionData(request: Request) {
   const session = await getSession(request.headers.get('Cookie'))
   return {
-    getWorld: () => {
-      try {
-        const world = session.get('world')
-        if (!WorldsArray.includes(world)) {
-          // @todo select a default
-          throw new Error(`World not an available option. [${world}]`)
-        }
-        return world
-      } catch (err) {
-        return WorldsArray.at(0)
-      }
-    },
-    getDataCenter: () => {
-      const dataCenter = session.get('data_center')
-      return DataCenterArray.includes(dataCenter) && dataCenter
-    }
+    getWorld:      () =>
+                     {
+                       try
+                         {
+                           const world = session.get('world')
+                           if(!WorldsArray.includes(world))
+                             {
+                               // @todo select a default
+                               throw new Error(`World not an available option. [${world}]`)
+                             }
+                           return world
+                         } catch(err)
+                         {
+                           return WorldsArray.at(0)
+                         }
+                     },
+    getDataCenter: () =>
+                     {
+                       const dataCenter = session.get('data_center')
+                       return DataCenterArray.includes(dataCenter) && dataCenter
+                     }
   }
 }
 
--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,15 @@
 declare module '*.svg' {
-    import React = require('react');
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement> & {className?: string}>;
+  import React = require('react')
+  export const ReactComponent: React.FC<
+    React.SVGProps<SVGSVGElement> & { className?: string }
+  >
+}
+
+declare var process: {
+  env: {
+    NODE_ENV: 'development' | 'production'
+    SITE_NAME: string
+    SESSION_COOKIE_SECRET: string
+  }
+  exit: (code: number) => void
 }
