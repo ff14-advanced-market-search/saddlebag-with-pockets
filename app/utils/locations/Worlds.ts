e8f209abb0ee6767b817eaf998930a1a675b1ea7
Ben Webb <b-webb@outlook.com>
2022-11-24 18:36:50 +0000

    Remove china and japan worlds from lists

--- app/utils/locations/Worlds.ts
+++ app/utils/locations/Worlds.ts
@@ -55,88 +55,88 @@ export const WorldList: Record<string, Array<{ name: string }>> = {
     { name: 'Alpha' },
     { name: 'Raiden' }
   ],
-  Elemental: [
-    { name: 'Aegis' },
-    { name: 'Atomos' },
-    { name: 'Carbuncle' },
-    { name: 'Garuda' },
-    { name: 'Gungnir' },
-    { name: 'Kujata' },
-    { name: 'Tonberry' },
-    { name: 'Typhon' }
-  ],
-  Gaia: [
-    { name: 'Alexander' },
-    { name: 'Bahamut' },
-    { name: 'Durandal' },
-    { name: 'Fenrir' },
-    { name: 'Ifrit' },
-    { name: 'Ridill' },
-    { name: 'Tiamat' },
-    { name: 'Ultima' }
-  ],
-  Mana: [
-    { name: 'Anima' },
-    { name: 'Asura' },
-    { name: 'Chocobo' },
-    { name: 'Hades' },
-    { name: 'Ixion' },
-    { name: 'Masamune' },
-    { name: 'Pandaemonium' },
-    { name: 'Titan' }
-  ],
-  Meteor: [
-    { name: 'Belias' },
-    { name: 'Mandragora' },
-    { name: 'Ramuh' },
-    { name: 'Shinryu' },
-    { name: 'Unicorn' },
-    { name: 'Valefor' },
-    { name: 'Yojimbo' },
-    { name: 'Zeromus' }
-  ],
+  // Elemental: [
+  //   { name: 'Aegis' },
+  //   { name: 'Atomos' },
+  //   { name: 'Carbuncle' },
+  //   { name: 'Garuda' },
+  //   { name: 'Gungnir' },
+  //   { name: 'Kujata' },
+  //   { name: 'Tonberry' },
+  //   { name: 'Typhon' }
+  // ],
+  // Gaia: [
+  //   { name: 'Alexander' },
+  //   { name: 'Bahamut' },
+  //   { name: 'Durandal' },
+  //   { name: 'Fenrir' },
+  //   { name: 'Ifrit' },
+  //   { name: 'Ridill' },
+  //   { name: 'Tiamat' },
+  //   { name: 'Ultima' }
+  // ],
+  // Mana: [
+  //   { name: 'Anima' },
+  //   { name: 'Asura' },
+  //   { name: 'Chocobo' },
+  //   { name: 'Hades' },
+  //   { name: 'Ixion' },
+  //   { name: 'Masamune' },
+  //   { name: 'Pandaemonium' },
+  //   { name: 'Titan' }
+  // ],
+  // Meteor: [
+  //   { name: 'Belias' },
+  //   { name: 'Mandragora' },
+  //   { name: 'Ramuh' },
+  //   { name: 'Shinryu' },
+  //   { name: 'Unicorn' },
+  //   { name: 'Valefor' },
+  //   { name: 'Yojimbo' },
+  //   { name: 'Zeromus' }
+  // ],
   Materia: [
     { name: 'Bismarck' },
     { name: 'Ravana' },
     { name: 'Sephirot' },
     { name: 'Sophia' },
     { name: 'Zurvan' }
-  ],
-  LuXingNiao: [
-    { name: 'HongYuHai' },
-    { name: 'ShenYiZhiDi' },
-    { name: 'LaNuoXiYa' },
-    { name: 'HuanYingQunDao' },
-    { name: 'MengYaChi' },
-    { name: 'YuZhouHeYin' },
-    { name: 'WoXianXiRan' }
-  ],
-  MoGuLi: [
-    { name: 'BaiYinXiang' },
-    { name: 'BaiJinHuanXiang' },
-    { name: 'ShenQuanHen' },
-    { name: 'ChaoFengTing' },
-    { name: 'LvRenZhanQiao' },
-    { name: 'FuXiaoZhiJian' },
-    { name: 'Longchaoshendian' },
-    { name: 'MengYuBaoJing' }
-  ],
-  MaoXiaoPang: [
-    { name: 'ZiShuiZhanQiao' },
-    { name: 'YanXia' },
-    { name: 'JingYuZhuangYuan' },
-    { name: 'MoDuNa' },
-    { name: 'HaiMaoChaWu' },
-    { name: 'RouFengHaiWan' },
-    { name: 'HuPoYuan' }
-  ],
-  DouDouChai: [
-    { name: 'ShuiJingTa' },
-    { name: 'YinLeiHu' },
-    { name: 'TaiYangHaiAn' },
-    { name: 'YiXiuJiaDe' },
-    { name: 'HongChaChuan' }
   ]
+  // LuXingNiao: [
+  //   { name: 'HongYuHai' },
+  //   { name: 'ShenYiZhiDi' },
+  //   { name: 'LaNuoXiYa' },
+  //   { name: 'HuanYingQunDao' },
+  //   { name: 'MengYaChi' },
+  //   { name: 'YuZhouHeYin' },
+  //   { name: 'WoXianXiRan' }
+  // ],
+  // MoGuLi: [
+  //   { name: 'BaiYinXiang' },
+  //   { name: 'BaiJinHuanXiang' },
+  //   { name: 'ShenQuanHen' },
+  //   { name: 'ChaoFengTing' },
+  //   { name: 'LvRenZhanQiao' },
+  //   { name: 'FuXiaoZhiJian' },
+  //   { name: 'Longchaoshendian' },
+  //   { name: 'MengYuBaoJing' }
+  // ],
+  // MaoXiaoPang: [
+  //   { name: 'ZiShuiZhanQiao' },
+  //   { name: 'YanXia' },
+  //   { name: 'JingYuZhuangYuan' },
+  //   { name: 'MoDuNa' },
+  //   { name: 'HaiMaoChaWu' },
+  //   { name: 'RouFengHaiWan' },
+  //   { name: 'HuPoYuan' }
+  // ],
+  // DouDouChai: [
+  //   { name: 'ShuiJingTa' },
+  //   { name: 'YinLeiHu' },
+  //   { name: 'TaiYangHaiAn' },
+  //   { name: 'YiXiuJiaDe' },
+  //   { name: 'HongChaChuan' }
+  // ]
 }
 
 export type WorldsList = typeof WorldList
