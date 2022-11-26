a3818e792013e1cee8aa5ca0bbc1036f4b83575c
cohenaj194 <cohenaj194@gmail.com>
2022-11-25 19:49:25 -0500

    add link to wiki on undercut alerts

--- app/components/navigation/sidebar/Sidebar.tsx
+++ app/components/navigation/sidebar/Sidebar.tsx
@@ -45,6 +45,12 @@ const navigation = [
     icon: DocumentSearchIcon
   },
   {
+    name: 'Undercut Discord Alerts',
+    href: 'https://github.com/ff14-advanced-market-search/saddlebag-with-pockets/wiki/Undercut-Alerts-Alpha',
+    icon: DocumentSearchIcon,
+    external: true
+  },
+  {
     name: 'Qualified Sale Alerts',
     href: 'https://www.patreon.com/indopan',
     icon: DocumentSearchIcon,
