3833a7b260dde3f2d409cde8f48d1d790ff90dcc
Ben Webb <b-webb@outlook.com>
2022-12-04 20:41:38 +0000

    Add undercut alert base response

--- app/routes/undercut/index.tsx
+++ app/routes/undercut/index.tsx
@@ -1,5 +1,5 @@
 import { useActionData, useTransition } from '@remix-run/react'
-import { ContentContainer, PageWrapper } from '~/components/Common'
+import { ContentContainer, PageWrapper, Title } from '~/components/Common'
 import { InputWithLabel } from '~/components/form/InputWithLabel'
 import SmallFormContainer from '~/components/form/SmallFormContainer'
 import type { ActionFunction } from '@remix-run/cloudflare'
@@ -7,6 +7,8 @@ import { json } from '@remix-run/cloudflare'
 import GetSellerId from '~/requests/GetSellerId'
 import { getUserSessionData } from '~/sessions'
 import ItemSelect from '~/components/form/select/ItemSelect'
+import { useState } from 'react'
+import { SubmitButton } from '~/components/form/SubmitButton'
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
@@ -49,13 +51,22 @@ export const action: ActionFunction = async ({ request }) => {
 const Index = () => {
   const transition = useTransition()
   const results = useActionData()
+  const [info, setInfo] = useState<{
+    addIds: Array<string>
+    removeIds: Array<string>
+    hqOnly: boolean
+  }>({
+    addIds: [],
+    removeIds: [],
+    hqOnly: false
+  })
 
   const onSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (transition.state === 'submitting') {
       e.preventDefault()
     }
   }
-  console.log(results)
+
   const error =
     results && results.exception
       ? results.exception
@@ -63,14 +74,71 @@ const Index = () => {
       ? 'Seller not found'
       : undefined
 
-  if (results && results.data) {
-    const jsonData = `{\n  "seller_id": "${results.data.seller_id}",\n  "server": "${results.homeServer}",\n  "add_ids": [],\n  "ignore_ids": [],\n  "hq_only": false\n}`
+  if (results && results.data && !error) {
+    const jsonData = `{\n  "seller_id": "${
+      results?.data.seller_id ??
+      '775e2c68ac04f1da582bfddd3d20be846b31201c7bda8d9187701c47474b5cb2'
+    }",\n  "server": "${
+      results?.homeServer ?? 'Adamantoise'
+    }",\n  "add_ids": [${info.addIds.join(
+      ','
+    )}],\n  "ignore_ids": [${info.removeIds.join(
+      ','
+    )}],\n  "hq_only": ${info.hqOnly.toString()}\n}`
     return (
       <PageWrapper>
         <ContentContainer>
-          <pre>
-            <code>{jsonData}</code>
-          </pre>
+          <div className="flex flex-col my-2 gap-2">
+            <Title title="Discord Undercut payload" />
+            <p className="italic text-sm text-grey-500 mb=1">
+              Copy the below text to your clipboard and use it in our{' '}
+              <a
+                className="underline"
+                href="https://discord.gg/836C8wDVNq"
+                target="_blank"
+                rel="noreferrer">
+                discord server
+              </a>{' '}
+              to activate{' '}
+              <a
+                className="underline"
+                href="https://github.com/ff14-advanced-market-search/saddlebag-with-pockets/wiki/Undercut-Alerts---Alpha-version"
+                target="_blank"
+                rel="noreferrer">
+                patreon undercut alerts.
+              </a>
+            </p>
+            <div className="flex justify-center items-center max-w-fit">
+              <label htmlFor="hq-only">High Quality Only</label>
+              <input
+                className="ml-2 rounded p-1"
+                id="hq-only"
+                type="checkbox"
+                checked={info.hqOnly}
+                onChange={() =>
+                  setInfo((state) => ({ ...state, hqOnly: !state.hqOnly }))
+                }
+              />
+            </div>
+
+            <pre className="overflow-x-scroll bg-slate-700 text-gray-200 p-3 rounded">
+              <code>{jsonData}</code>
+            </pre>
+            <div className="max-w-fit my-2">
+              <SubmitButton
+                title="Copy to clipboard"
+                type="button"
+                onClick={async () => {
+                  if (!window.isSecureContext) {
+                    alert('Unable to copy.')
+                    return
+                  }
+                  await navigator.clipboard.writeText(jsonData)
+                  alert('Copied to clipboard')
+                }}
+              />
+            </div>
+          </div>
         </ContentContainer>
       </PageWrapper>
     )
