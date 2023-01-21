be99f851574be70314bb968059f987dd700e8110
Ben Webb <b-webb@outlook.com>
2023-01-21 19:53:59 +0000

    Remove out of stock scan page, and add to redirect

--- app/utils/redirectOnPath.ts
+++ app/utils/redirectOnPath.ts
@@ -4,6 +4,8 @@ export const COMMODITY_SCAN_PATH =
   '/queries/full-scan?minimumStackSize=2&minimumProfitAmount=1000&pricePerUnit=1000'
 export const FAST_SCAN_PATH =
   '/queries/full-scan?salesAmount=20&minimumProfitAmount=500&pricePerUnit=500'
+export const OUT_OF_STOCK_PATH =
+  '/queries/full-scan?hours=99&salesAmount=2&ROI=99&minimumProfitAmount=100&pricePerUnit=100&filters=1,2,3,4,7&hQChecked=true&outOfStockChecked=true'
 
 export const redirectOnPath = (path: string): RedirectOnPath => {
   const needsRedirect = replacedPages.find(
@@ -19,5 +21,6 @@ export const redirectOnPath = (path: string): RedirectOnPath => {
 
 const replacedPages: Array<{ oldPath: string; newPath: string }> = [
   { oldPath: '/queries/commodity-scan', newPath: COMMODITY_SCAN_PATH },
-  { oldPath: '/queries/fast-scan', newPath: FAST_SCAN_PATH }
+  { oldPath: '/queries/fast-scan', newPath: FAST_SCAN_PATH },
+  { oldPath: '/queries/out-stock-scan', newPath: OUT_OF_STOCK_PATH }
 ]
