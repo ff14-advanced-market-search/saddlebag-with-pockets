0b2353a930daa9e190770f3e1ace5d741251c398
Ben Webb <b-webb@outlook.com>
2023-01-30 22:13:24 +0000

    Update ffxiv queries page

--- app/routes/queries/index.tsx
+++ app/routes/queries/index.tsx
@@ -153,22 +153,22 @@ export default function Index() {
   return (
     <>
       <main className="flex-1">
-        <div className="rounded-md bg-blue-50 p-4 m-4 border-2 border-blue-100">
+        <div className="rounded-md bg-blue-50 p-4 m-4 border-2 border-blue-100 dark:bg-blue-900 dark:border-blue-900">
           <div className="flex">
             <div className="flex-shrink-0">
               <InformationCircleIcon
-                className="h-5 w-5 text-blue-400"
+                className="h-5 w-5 text-blue-400 dark:text-gray-200"
                 aria-hidden="true"
               />
             </div>
             <div className="ml-3 flex-1 md:flex md:justify-between">
-              <p className="text-sm text-blue-700">
+              <p className="text-sm text-blue-700 dark:text-gray-200">
                 Welcome to the new Saddlebag Exchange!{' '}
               </p>
               <p className="mt-3 text-sm md:mt-0 md:ml-6">
                 <a
                   href="https://discord.gg/836C8wDVNq"
-                  className="whitespace-nowrap font-medium text-blue-700 hover:text-blue-600">
+                  className="whitespace-nowrap font-medium text-blue-700 dark:text-gray-100 hover:text-blue-600 dark:hover:text-blue-100">
                   Discord
                   <span aria-hidden="true"> &rarr;</span>
                 </a>
@@ -178,7 +178,7 @@ export default function Index() {
         </div>
         <div className="py-6">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            <h1 className="text-2xl font-semibold text-blue-900">
+            <h1 className="text-2xl font-semibold text-blue-900 dark:text-gray-100">
               Recommended Queries
             </h1>
             <div
@@ -187,7 +187,7 @@ export default function Index() {
                 return (
                   <div
                     key={query.name}
-                    className={`group relative rounded-xl border border-slate-200 dark:border-slate-800`}>
+                    className={`group relative rounded-xl border border-slate-200 dark:border-0 dark:bg-slate-700`}>
                     <div
                       className={`absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.blue.50)),var(--quick-links-hover-bg,theme(colors.blue.50)))_padding-box,linear-gradient(to_top,theme(colors.yellow.400),theme(colors.yellow.400),theme(colors.blue.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]`}
                     />
