522ab30346ed2441ebd23b05f9448c463ee14dc1
Ben Webb <b-webb@outlook.com>
2023-01-30 13:44:29 +0000

    Update options pages with dark mode

--- app/routes/options/index.tsx
+++ app/routes/options/index.tsx
@@ -94,15 +94,17 @@ const OptionSection = ({
             <div className="md:grid md:grid-cols-3 md:gap-6">
               <div className="md:col-span-1">
                 <div className="px-4 sm:px-0">
-                  <h3 className="text-lg font-medium leading-6 text-gray-900">
+                  <h3 className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-300">
                     {title}
                   </h3>
-                  <p className="mt-1 text-sm text-gray-600">{description}</p>
+                  <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                    {description}
+                  </p>
                 </div>
               </div>
               <div className="mt-5 md:mt-0 md:col-span-2">
                 <div className="shadow sm:rounded-md sm:overflow-hidden">
-                  <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                  <div className="px-4 py-5 bg-white dark:bg-slate-600 space-y-6 sm:p-6">
                     {children}
                   </div>
                 </div>
@@ -159,12 +161,14 @@ export default function () {
         <Form method="post">
           <div className="py-6">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              <h1 className="text-2xl font-semibold text-gray-900">Options</h1>
+              <h1 className="text-2xl font-semibold text-gray-900 dark:text-gray-200">
+                Options
+              </h1>
             </div>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
               <div className="lg:flex lg:items-center lg:justify-between">
                 <div className="flex-1 min-w-0">
-                  <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                  <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate dark:text-gray-300">
                     Site Configuration
                   </h2>
                 </div>
