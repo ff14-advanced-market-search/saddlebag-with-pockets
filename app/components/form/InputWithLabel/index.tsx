d8446db59ed01bb231de5aa477de8fe8cd1ad2e6
Ben Webb <b-webb@outlook.com>
2023-01-30 21:35:50 +0000

    Update a few components with dark theme

--- app/components/form/InputWithLabel/index.tsx
+++ app/components/form/InputWithLabel/index.tsx
@@ -23,12 +23,12 @@ export const InputWithLabel = ({
     <div className="relative flex flex-1 items-center gap-1">
       <label
         htmlFor={labelTitle}
-        className="block text-sm font-medium text-gray-700 dark:text-grey-100">
+        className="block text-sm font-medium text-gray-700 dark:text-gray-100">
         {labelTitle}
       </label>
       {toolTip && <ToolTip data={toolTip} />}
     </div>
-    <div className="mt-1 flex rounded-md shadow-sm border border-gray-300">
+    <div className="mt-1 flex rounded-md shadow-sm border border-gray-300 dark:border-gray-400">
       <input
         id={labelTitle}
         {...rest}
@@ -36,11 +36,11 @@ export const InputWithLabel = ({
           inputTag
             ? 'rounded-l-md border-gray-300 border-0 border-r'
             : 'border-0 rounded-md'
-        } focus:ring-blue-500 focus:border-2 focus:border-blue-500`}
+        } focus:ring-blue-500 focus:border-2 focus:border-blue-500 dark:border-gray-400 dark:text-gray-100 dark:bg-gray-600`}
       />
       {inputTag && (
         <span
-          className={`inline-flex items-center justify-center text-center px-3 rounded-r-md bg-gray-50 text-gray-500 sm:text-sm`}>
+          className={`inline-flex items-center justify-center text-center px-3 rounded-r-md bg-gray-50 text-gray-500 sm:text-sm dark:text-gray-300 dark:bg-gray-700`}>
           {inputTag}
         </span>
       )}
