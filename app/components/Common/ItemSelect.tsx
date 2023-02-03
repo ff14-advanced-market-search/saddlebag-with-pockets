978959ff24600e1ec76c3f15463addb31b40765d
Ben Webb <b-webb@outlook.com>
2023-02-03 09:31:08 +0000

    Update Item history pages with dark theme

--- app/components/Common/ItemSelect.tsx
+++ app/components/Common/ItemSelect.tsx
@@ -59,7 +59,7 @@ const ItemSelect = ({
                   onSelectChange(undefined)
                 }
               }}
-              className="flex-1 min-w-0 block w-full px-3 py-2 rounded-none rounded-l-md focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md dark:border-gray-400 dark:text-gray-100 dark:bg-gray-600 dark:placeholder-gray-300"
+              className="flex-1 min-w-0 block w-full px-3 py-2 rounded-none rounded-l-md focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md dark:border-gray-400 dark:text-gray-100 dark:bg-gray-600 dark:placeholder-gray-400"
             />
             <span
               className={`inline-flex items-center px-3 rounded-r-md border border-l-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm dark:text-gray-300 dark:bg-gray-700`}>
