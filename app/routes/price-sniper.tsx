1ffde405c7bb6b631bcce3d7c9ec6db2902849db
Ben Webb <b-webb@outlook.com>
2023-03-08 08:04:02 +0000

    Tidy up other components

--- app/routes/price-sniper.tsx
+++ app/routes/price-sniper.tsx
@@ -1,9 +1,6 @@
 import { Outlet } from '@remix-run/react'
-import type { SelectWorldInputFields } from './options/index'
 
-export type { SelectWorldInputFields }
-
-export default function Options() {
+export default function PriceSniper() {
   return (
     <div>
       <Outlet />
--- app/routes/undercut.tsx
+++ app/routes/undercut.tsx
@@ -1,7 +1,4 @@
 import { Outlet } from '@remix-run/react'
-import type { SelectWorldInputFields } from './options/index'
-
-export type { SelectWorldInputFields }
 
 export default function Options() {
   return (
