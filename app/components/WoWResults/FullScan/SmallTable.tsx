4c88b1ab29302cd3dce3e3349ae9999c83e2cfd9
Ben Webb <b-webb@outlook.com>
2023-04-15 23:55:23 +0100

    Center table text

--- app/components/WoWResults/FullScan/SmallTable.tsx
+++ app/components/WoWResults/FullScan/SmallTable.tsx
@@ -139,7 +139,7 @@ function DesktopTable({
                         onClick={header.column.getToggleSortingHandler()}
                         className={classNames(
                           header.column.getCanSort() ? 'cursor-pointer' : '',
-                          `px-3 py-3.5 sticky bg-gray-50 top-0 text-left text-sm font-semibold text-gray-900 dark:text-gray-100 dark:bg-gray-600`
+                          `px-3 py-3.5 sticky bg-gray-50 top-0 text-sm font-semibold text-gray-900 dark:text-gray-100 dark:bg-gray-600 text-center`
                         )}
                         key={header.id}>
                         <div className={`group inline-flex`}>
