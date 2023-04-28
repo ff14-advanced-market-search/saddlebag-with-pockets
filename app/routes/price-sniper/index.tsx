924616c509741a647396514f435f2dfe7b80f551
oldben87 <51456734+oldben87@users.noreply.github.com>
2023-04-28 22:27:44 +0100

    Task/prive quantity alerts (#231)
    * Toggle between price or quantity alerts
    
    * Make radio inline
    
    * Update commands for the ffxiv price alerts

--- app/routes/price-sniper/index.tsx
+++ app/routes/price-sniper/index.tsx
@@ -36,14 +36,16 @@ interface SubFormItem {
   price: number
 }
 
-export const parseJSONInput = (input: Array<Auction>) => {
+export const parseJSONInput = (input: Array<Auction>, isPrice = true) => {
   if (!input.length) {
     return ''
   }
 
   return `,\n  "user_auctions": [${input
     .map(({ itemID, price, desiredState, hq }) => {
-      return `\n    { "itemID": ${itemID}, "price": ${price}, "desired_state": "${desiredState}", "hq": ${hq} }`
+      return `\n    { "itemID": ${itemID}, "${
+        isPrice ? 'price' : 'quantity'
+      }": ${price}, "desired_state": "${desiredState}", "hq": ${hq} }`
     })
     .join(',')}\n  ]`
 }
@@ -56,17 +58,24 @@ const Index = () => {
   }>({ server: world, userAuctions: [] })
   const [subForm, setSubForm] = useState<SubFormItem | null>(null)
   const [error, setError] = useState<string | undefined>(undefined)
+  const [isPrice, setIsPrice] = useState(true)
 
   const jsonToDisplay = `{\n  "home_server": "${
     jsonData.server
-  }"${parseJSONInput(jsonData.userAuctions)}\n}`
+  }"${parseJSONInput(jsonData.userAuctions, isPrice)}\n}`
+
+  const isPriceValue = isPrice ? 'price' : 'quantity'
+
+  const description = `To setup ${isPriceValue} alerts search, enter items and a desired
+  ${isPriceValue}. Copy and paste the below input for the Discord Bot in the
+  Saddlebage Exchange discord server.`
 
   return (
     <PageWrapper>
       <>
         <SmallFormContainer
           title="Input for Price Sniper Alerts"
-          description="To setup price sniper alerts search, enter items and a desired price. Copy and paste the below input for the Discord Bot in the Saddlebage Exchange discord server for the discord bot commands '/ff price-register' or '/ff price-update'!"
+          description={description}
           error={error}
           onClick={(e) => {
             e.preventDefault()
@@ -120,8 +129,39 @@ const Index = () => {
 
             {subForm && (
               <div className="sm:px-4">
+                <div
+                  className="my-2"
+                  onChange={(event: React.SyntheticEvent<EventTarget>) => {
+                    const value = (event.target as HTMLInputElement).value
+                    if (value === 'price') setIsPrice(true)
+                    if (value === 'quantity') setIsPrice(false)
+                  }}>
+                  <Label>Alert on Price or Quantity of item: </Label>
+                  <div className="flex gap-2">
+                    <Label htmlFor="radio-price">
+                      <input
+                        id="radio-price"
+                        type="radio"
+                        value="price"
+                        name="price-quantity"
+                        defaultChecked={isPrice === true}
+                      />{' '}
+                      Price
+                    </Label>
+                    <Label htmlFor="radio-quantity">
+                      <input
+                        id="radio-quantity"
+                        type="radio"
+                        value="quantity"
+                        name="price-quantity"
+                        defaultChecked={isPrice === false}
+                      />{' '}
+                      Quantity
+                    </Label>
+                  </div>
+                </div>
                 <InputWithLabel
-                  labelTitle="Price to alert on"
+                  labelTitle={`${isPrice ? 'Price' : 'Quantity'} to alert on`}
                   type="number"
                   inputTag="Gil"
                   min={0}
@@ -170,7 +210,7 @@ const Index = () => {
                       })
                     }
                   }}>
-                  <Label> Alert when price is: </Label>
+                  <Label> Alert when {isPriceValue} is: </Label>
                   <Label htmlFor="radio-below">
                     <input
                       id="radio-below"
@@ -179,7 +219,7 @@ const Index = () => {
                       name="alertOn"
                       defaultChecked={subForm.desiredState === 'below'}
                     />{' '}
-                    Below {subForm.price} Gil
+                    Below {`${subForm.price} ${isPrice ? 'Gil' : ''}`}
                   </Label>
                   <Label htmlFor="radio-above">
                     <input
@@ -189,7 +229,7 @@ const Index = () => {
                       name="alertOn"
                       defaultChecked={subForm.desiredState === 'above'}
                     />{' '}
-                    Above {subForm.price} Gil
+                    Above {`${subForm.price} ${isPrice ? 'Gil' : ''}`}
                   </Label>
                 </div>
                 <HQCheckbox
@@ -208,11 +248,25 @@ const Index = () => {
 
         <div className="max-w-4xl mx-auto px-4">
           <CodeBlock
-            title="Input for price sniper alert"
+            title={`Input for ${isPriceValue} sniper alert`}
             buttonTitle="Copy to clipboard"
             codeString={jsonToDisplay}
-            onClick={() => alert('Copied to clipboard!')}
-          />
+            onClick={() => alert('Copied to clipboard!')}>
+            <p className="italic text-sm text-blue-900 py-2 dark:text-gray-100">
+              For the discord bot, use the{' '}
+              {isPrice ? (
+                <span>
+                  commands <b>'/ff price-register'</b> or{' '}
+                  <b>'/ff price-update'</b>
+                </span>
+              ) : (
+                <span>
+                  command <b>'/ff quantity-register'</b>
+                </span>
+              )}
+              !
+            </p>
+          </CodeBlock>
         </div>
       </>
     </PageWrapper>
