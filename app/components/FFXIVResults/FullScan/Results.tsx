f007358d442a8f5e9c0eba438b4f34a4ffbe6cd1
cohenaj194 <cohenaj194@gmail.com>
2023-05-30 01:21:03 -0400

    update average name

--- app/components/FFXIVResults/FullScan/Results.tsx
+++ app/components/FFXIVResults/FullScan/Results.tsx
@@ -157,7 +157,7 @@ const Results = ({ rows }: ResultTableProps) => {
     }),
 
     columnHelper.accessor('sale_rates', {
-      header: 'Avg Sales Per Hour',
+      header: 'Average Sales Per Hour',
       cell: (info) => info.getValue().toLocaleString()
     }),
     columnHelper.accessor('server', {
