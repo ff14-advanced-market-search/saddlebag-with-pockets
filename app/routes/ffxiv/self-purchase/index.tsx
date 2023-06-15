ab6fe7132aad7eedb2d897f0df2343634f63e844
Ben Webb <b-webb@outlook.com>
2023-06-15 18:04:11 +0100

    Add server picker for the self purchase form

--- app/routes/ffxiv/self-purchase/index.tsx
+++ app/routes/ffxiv/self-purchase/index.tsx
@@ -1,16 +1,19 @@
 import type {
   ActionFunction,
-  ErrorBoundaryComponent
+  ErrorBoundaryComponent,
+  LoaderFunction
 } from '@remix-run/cloudflare'
 import { json } from '@remix-run/cloudflare'
-import { useActionData, useTransition } from '@remix-run/react'
+import { useActionData, useLoaderData, useTransition } from '@remix-run/react'
 import { PageWrapper, Title } from '~/components/Common'
 import NoResults from '~/components/Common/NoResults'
+import TitleTooltip from '~/components/Common/TitleTooltip'
 import DateCell from '~/components/FFXIVResults/FullScan/DateCell'
 import FullTable from '~/components/Tables/FullTable'
 import MobileTable from '~/components/WoWResults/FullScan/MobileTable'
 import { InputWithLabel } from '~/components/form/InputWithLabel'
 import SmallFormContainer from '~/components/form/SmallFormContainer'
+import { SelectWorld } from '~/components/form/select/SelectWorld/SelectWorld'
 import type { ColumnList } from '~/components/types'
 import { ErrorBoundary as ErrorBounds } from '~/components/utilities/ErrorBoundary'
 import ItemDataLink from '~/components/utilities/ItemDataLink'
@@ -18,31 +21,50 @@ import type { SelfPurchaseResults } from '~/requests/FFXIV/self-purchase'
 import selfPurchaseRequest from '~/requests/FFXIV/self-purchase'
 import type { SelfPurchase } from '~/requests/FFXIV/self-purchase'
 import { getUserSessionData } from '~/sessions'
+import WorldsMap from '~/utils/locations/Worlds'
 
 export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => (
   <ErrorBounds error={error} />
 )
 
-export const action: ActionFunction = async ({ request }) => {
-  const formData = await request.formData()
+export const loader: LoaderFunction = async ({ request }) => {
   const session = await getUserSessionData(request)
 
+  return json({
+    server: session.getWorld(),
+    dataCenter: session.getDataCenter()
+  })
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData()
+
   const playerName = formData.get('playerName')
+  const server = formData.get('world')
 
   if (!playerName || typeof playerName !== 'string' || !playerName.trim()) {
     return json({ exception: 'No player name provided' })
   }
 
+  if (!server || typeof server !== 'string' || !server.trim()) {
+    return json({ exception: 'No server provided' })
+  }
+
   return await selfPurchaseRequest({
-    server: session.getWorld(),
+    server: server.trim(),
     playerName: playerName.trim()
   })
 }
 
 export default function Index() {
   const transition = useTransition()
+  const loaderData = useLoaderData<{ server: string; dataCenter: string }>()
   const results = useActionData<SelfPurchaseResults>()
   const loading = transition.state === 'submitting'
+
+  const worldList = WorldsMap.get(loaderData.dataCenter)
+
+  const worlds = worldList ? worldList : []
 
   const error =
     results && 'exception' in results ? results.exception : undefined
@@ -69,6 +91,17 @@ export default function Index() {
           }
         }}>
         <div className="pt-2">
+          <TitleTooltip
+            title="Choose world to search"
+            toolTip="Choose a world in your data center"
+            relative
+          />
+          <SelectWorld
+            dataCenter={loaderData.dataCenter}
+            world={loaderData.server}
+            onSelect={() => {}}
+            worlds={worlds}
+          />
           <InputWithLabel
             type="text"
             name="playerName"
