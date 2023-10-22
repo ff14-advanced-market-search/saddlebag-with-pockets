2fcd57bb0f433ae0fb4e73625ef773e6a20a0c9d
Ben Webb <b-webb@outlook.com>
2023-10-22 12:52:59 +0100

    Use callback to complete file write

--- scripts/buildItemsList.js
+++ scripts/buildItemsList.js
@@ -1,45 +1,52 @@
-const https = require('https')
-const fs = require('fs')
+const { get } = require('https')
+const { writeFile } = require('fs')
 
 const ADDRESS =
   'https://raw.githubusercontent.com/ffxiv-teamcraft/ffxiv-teamcraft/staging/libs/data/src/lib/json/items.json'
 const FILE_PATH = './app/utils/items/items.ts'
 console.log('Fetching items from:', ADDRESS)
 
-https
-  .get(ADDRESS, (response) => {
-    console.log('Response received:', response.statusCode)
-    const data = []
+get(ADDRESS, (response) => {
+  console.log('Response received:', response.statusCode)
+  const data = []
 
-    response.on('data', (chunk) => {
-      data.push(chunk)
-    })
-    response.on('end', () => {
-      console.log('Building file...')
-      const raw = JSON.parse(Buffer.concat(data).toString())
-      const result = {}
-      Object.entries(raw).forEach(([id, { en }]) => {
-        if (en.length) {
-          result[id] = en.replace('\u00a0', ' ')
-        }
-      })
-
-      console.log('Writing file:', FILE_PATH)
-
-      fs.writeFile(
-        FILE_PATH,
-        'export const itemsMap: Record<string, string> = ' +
-          JSON.stringify(result, null, 2),
-        function (err) {
-          if (err) throw err
-        }
-      )
-      console.log('Items successfully written')
-      process.exit(0)
-    })
+  response.on('data', (chunk) => {
+    data.push(chunk)
   })
-  .on('error', (error) => {
-    console.error(error.message)
+  response.on('end', () => {
+    const raw = JSON.parse(Buffer.concat(data).toString())
 
-    process.exit(1)
+    const itemsEntries = Object.entries(raw)
+
+    console.log('Items recieved:', itemsEntries.length)
+
+    console.log('Building file...')
+
+    const result = {}
+
+    itemsEntries.forEach(([id, { en }]) => {
+      if (en.length) {
+        result[id] = en.replace('\u00a0', ' ')
+      }
+    })
+
+    console.log('Writing file:', FILE_PATH)
+
+    writeFile(
+      FILE_PATH,
+      'export const itemsMap: Record<string, string> = ' +
+        JSON.stringify(result, null, 2),
+      function (err) {
+        if (err) {
+          throw err
+        }
+        console.log('Items successfully written:', Object.keys(result).length)
+        process.exit(0)
+      }
+    )
   })
+}).on('error', (error) => {
+  console.error(error.message)
+
+  process.exit(1)
+})
