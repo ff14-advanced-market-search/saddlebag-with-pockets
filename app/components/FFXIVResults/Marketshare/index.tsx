d79904ef73b153016d7226edf451758d7c2dbbd1
cohenaj194 <cohenaj194@gmail.com>
2024-05-01 16:41:51 -0400

    lint

--- app/components/FFXIVResults/Marketshare/index.tsx
+++ app/components/FFXIVResults/Marketshare/index.tsx
@@ -210,8 +210,9 @@ export const Results = ({
   const [globalFilter, setGlobalFilter] = useState('')
   const chartData = getChartData(data, sortBy)
 
-  const sortByTitleValue = sortByOptions.find(({ value }) => value === sortBy)
-    ?.label
+  const sortByTitleValue = sortByOptions.find(
+    ({ value }) => value === sortBy
+  )?.label
 
   const chartTitle = sortByTitleValue
     ? `Marketshare Overview - ${sortByTitleValue}`
