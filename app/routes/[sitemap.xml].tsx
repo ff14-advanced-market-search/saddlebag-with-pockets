1a99bdc1761a30b94f5debc20c5b96260e31582f
cohenaj194 <cohenaj194@gmail.com>
2024-05-02 00:18:07 -0400

    add sitemap to blog

--- app/routes/[sitemap.xml].tsx
+++ app/routes/[sitemap.xml].tsx
@@ -191,7 +191,82 @@ export const loader: LoaderFunction = async () => {
   <loc>https://saddlebagexchange.com/blog/wow/howtocommodityshortage</loc>
   <lastmod>2024-04-27T00:27:48+00:00</lastmod>
   <priority>0.64</priority>
-</url>  
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs1</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs2</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs3</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs4</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs5</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs6</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs7</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs8</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs9</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs10</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs11</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs12</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs13</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs14</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://saddlebagexchange.com/blog/ffxiv/bs15</loc>
+  <lastmod>2024-04-27T00:27:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
 </urlset>`
   return new Response(Sitemap, {
     status: 200,
