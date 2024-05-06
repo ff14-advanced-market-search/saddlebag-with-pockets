29575acab75eb29b08ff5f709cd4cdd87fc63562
cohenaj194 <cohenaj194@gmail.com>
2024-05-06 17:15:11 -0400

    add h2 title

---
+++ app/components/Common/TitleH2.tsx
@@ -0,0 +1,17 @@
+import { classNames } from '~/utils'
+
+export const TitleH2 = ({
+  title,
+  className = ''
+}: {
+  title: string
+  className?: string
+}) => (
+  <h2
+    className={classNames(
+      `text-2xl font-semibold text-blue-900 py-2 dark:text-gray-100`,
+      className
+    )}>
+    {title}
+  </h2>
+)
