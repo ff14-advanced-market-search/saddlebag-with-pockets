1db6374e6a4f3bddda4de6f17b167ee3fab9eeda
cohenaj194 <cohenaj194@gmail.com>
2024-07-06 20:00:37 -0400

    create extended-history file

---
+++ app/routes/ffxiv.extended-history.tsx
@@ -0,0 +1,14 @@
+const Foobar = () => {
+  return (
+    <div className={`m-12`}>
+      <main className="flex-1">
+        <h2 className={`text-2xl`}>Foobar</h2>
+        <p className={`text-prose`}>
+          Foobar
+        </p>
+      </main>
+    </div>
+  )
+}
+
+export default Foobar
