fade0ec77fc50dba4dc9529e6e13fe36956ebb92
Alex Cohen <cohenaj194@gmail.com>
2024-09-28 16:34:56 -0400

    Update [robots.txt].tsx

--- app/routes/[robots.txt].tsx
+++ app/routes/[robots.txt].tsx
@@ -2,12 +2,16 @@ import { LoaderFunction } from '@remix-run/cloudflare'
 
 export const loader: LoaderFunction = () => {
   const robotText = `
-  User-agent: Googlebot
-  Disallow: /nogooglebot/
-  Disallow: /build/
-
   User-agent: *
   Allow: /
+  Allow: /ffxiv
+  Allow: /queries
+  Allow: /wow
+  Allow: /queries/item-data/*
+  Allow: /wow/item-data/*
+
+  Disallow: /nogooglebot/
+  Disallow: /build/
 
   Sitemap: https://saddlebagexchange.com/sitemap.xml
   `
