00394237495789dd15c80cca8904a96fa2cab788
cohenaj194 <cohenaj194@gmail.com>
2024-11-09 13:48:05 -0500

    remove whitespace

--- app/routes/blog.ffxiv.bs1.tsx
+++ app/routes/blog.ffxiv.bs1.tsx
@@ -20,7 +20,6 @@ const HowtoCrossServerTradeinFFXIV = () => {
   return (
     <div className={`m-12`}>
       <Banner />
-
       <main className="flex-1">
         <h1 id="mastering-gil-earning-on-the-ffxiv-marketboard">
           Mastering Gil Earning on the FFXIV Marketboard
