eb5152c99108f8b7a5389918e0f02ad45cde16c4
cohenaj194 <cohenaj194@gmail.com>
2025-03-06 22:13:44 -0500

    fix path

--- app/requests/WoW/ShoppingList.ts
+++ app/requests/WoW/ShoppingList.ts
@@ -1,5 +1,5 @@
 import { address, UserAgent } from '~/requests/client/config'
-import type { WoWServerRegion } from '../WOWScan'
+import type { WoWServerRegion } from './WOWScan'
 
 interface WoWShoppingListProps {
   region: WoWServerRegion
