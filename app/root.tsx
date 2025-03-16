36a05cd3f9b10329498af08d69b89c6d9f9c99ef
cohenaj194 <cohenaj194@gmail.com>
2025-03-15 21:45:24 -0400

    use let not var

--- app/root.tsx
+++ app/root.tsx
@@ -309,7 +309,7 @@ function App() {
         ;(function (w, d, s, l, i) {
           w[l] = w[l] || []
           w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
-          var f = d.getElementsByTagName(s)[0],
+          let f = d.getElementsByTagName(s)[0],
             j = d.createElement(s),
             dl = l != 'dataLayer' ? '&l=' + l : ''
           j.async = true
