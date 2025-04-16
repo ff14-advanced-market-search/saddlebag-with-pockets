12fb9610468b8df18f9d2c75890f8f4e690ca29b
cohenaj194 <cohenaj194@gmail.com>
2025-04-16 10:25:35 -0400

    fat arrow

--- app/components/WoW/PriceQuantityAnalysis.tsx
+++ app/components/WoW/PriceQuantityAnalysis.tsx
@@ -21,7 +21,9 @@ export default function PriceQuantityAnalysis({
       <div className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow mb-4">
         <button
           type="button"
-          onClick={() => setShowPriceQuantityCharts(!showPriceQuantityCharts)}
+          onClick={() => {
+            setShowPriceQuantityCharts(!showPriceQuantityCharts)
+          }}
           className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md transform transition-all duration-200 hover:scale-105 flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
