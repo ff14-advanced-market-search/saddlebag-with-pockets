087b13fa3e9e55aa285261873ca09fb7b7fa6e09
cohenaj194 <cohenaj194@gmail.com>
2025-05-18 16:05:38 -0400

    add new wow item lists

--- app/utils/items/id_to_item.ts
+++ app/utils/items/id_to_item.ts
@@ -1,8 +1,12 @@
 import { ffxivItemsMap } from './ffxivItems'
 import { wowItemsMap } from './wowItems'
+import { wowPetsItemsMap } from './wowPetItems'
+import { wowStackableItemsMap } from './wowStackableItems'
 
 export const ffxivItems = Object.entries(ffxivItemsMap)
 export const wowItems = Object.entries(wowItemsMap)
+export const wowPetsItems = Object.entries(wowPetsItemsMap)
+export const wowStackableItems = Object.entries(wowStackableItemsMap)
 
 export const parseItemsForDataListSelect = ([value, label]: [
   string,
@@ -14,3 +18,5 @@ export const parseItemsForDataListSelect = ([value, label]: [
 
 export const ffxivItemsList = ffxivItems.map(parseItemsForDataListSelect)
 export const wowItemsList = wowItems.map(parseItemsForDataListSelect)
+export const wowPetItemsList = wowPetsItems.map(parseItemsForDataListSelect)
+export const wowStackableItemList = wowStackableItems.map(parseItemsForDataListSelect)
