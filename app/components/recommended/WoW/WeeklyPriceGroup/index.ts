91e4bfc41fb662d32243560bc42e5522d0536eec
cohenaj194 <cohenaj194@gmail.com>
2025-05-25 23:25:29 -0400

    add tww mounts

--- app/components/recommended/WoW/WeeklyPriceGroup/index.ts
+++ app/components/recommended/WoW/WeeklyPriceGroup/index.ts
@@ -5,10 +5,13 @@ import raidConsumables from './raidConsumables'
 import craftingMaterials from './craftingMaterials'
 import complete from './complete'
 import tier11_1 from './tier-11-1'
+import engMountTWW from './engMountTww'
+
 
 const recommendedConfigs = [
   tier11_1, // Tier 11.1 Complete Analysis
   complete, // test
+  engMountTWW, // Increases from TWW mount using old parts
   enchants,
   alchemy,
   gems,
