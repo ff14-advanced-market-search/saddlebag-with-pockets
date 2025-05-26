37b331b719a5f7db529a23f80c47d41b90217128
cohenaj194 <cohenaj194@gmail.com>
2025-05-25 23:44:09 -0400

    add potential furninishngs

--- app/components/recommended/WoW/WeeklyPriceGroup/index.ts
+++ app/components/recommended/WoW/WeeklyPriceGroup/index.ts
@@ -8,19 +8,21 @@ import tier11_1 from './tier-11-1'
 import engMountTWW from './engMountTwwParts'
 import engMountTwwOre from './engMountTwwOre'
 import dragonflight1 from './dragonflight1'
+import furnishingComponents from './furnishingComponents'
 
 const recommendedConfigs = [
   tier11_1, // Tier 11.1 Complete Analysis
   engMountTWW, // Increases from TWW mount using old parts
   engMountTwwOre, // Increases from TWW mount using old parts
   dragonflight1, // Dragonflight Market
+  furnishingComponents, // Midnight Expanse Furnishing Components
 
-  complete, // test
-  enchants,
-  alchemy,
-  gems,
-  raidConsumables,
-  craftingMaterials
+  // complete, // test
+  // enchants,
+  // alchemy,
+  // gems,
+  // raidConsumables,
+  // craftingMaterials
 ]
 
 export default recommendedConfigs
