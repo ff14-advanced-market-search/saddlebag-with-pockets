9cabfa797b0f04fb1c4d32bd3f3075dcefc144bc
cohenaj194 <cohenaj194@gmail.com>
2025-05-25 23:39:15 -0400

    added dragonflight

--- app/components/recommended/WoW/WeeklyPriceGroup/index.ts
+++ app/components/recommended/WoW/WeeklyPriceGroup/index.ts
@@ -7,11 +7,13 @@ import complete from './complete'
 import tier11_1 from './tier-11-1'
 import engMountTWW from './engMountTwwParts'
 import engMountTwwOre from './engMountTwwOre'
+import dragonflight1 from './dragonflight1'
 
 const recommendedConfigs = [
   tier11_1, // Tier 11.1 Complete Analysis
   engMountTWW, // Increases from TWW mount using old parts
   engMountTwwOre, // Increases from TWW mount using old parts
+  dragonflight1, // Dragonflight Market
 
   complete, // test
   enchants,
