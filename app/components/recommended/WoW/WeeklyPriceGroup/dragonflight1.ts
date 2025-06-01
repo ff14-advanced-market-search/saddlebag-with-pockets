17cd3e34c72b0ffd93e98a5777e5377ddaee55cb
cohenaj194 <cohenaj194@gmail.com>
2025-06-01 18:19:23 -0400

    fix names and order

--- app/components/recommended/WoW/WeeklyPriceGroup/dragonflight1.ts
+++ app/components/recommended/WoW/WeeklyPriceGroup/dragonflight1.ts
@@ -1,7 +1,7 @@
 const dragonflight1 = {
-  name: 'Dragonflight Market',
+  name: 'Complete Dragonflight Trade Goods Market',
   description:
-    'Limited dragonflight data 2023 - 2024, we started collecting data Jan 2023 and are missing the start of the expansion.',
+    'Limited dragonflight data 2023 - 2024, we started collecting data Jan 2023 and are missing the start of the expansion. This is everything we have on trade goods.',
   config: {
     region: 'NA',
     start_year: 2023,
--- app/components/recommended/WoW/WeeklyPriceGroup/index.ts
+++ app/components/recommended/WoW/WeeklyPriceGroup/index.ts
@@ -17,11 +17,11 @@ const recommendedConfigs = [
   consumablesTier11_0, // Consumables Tier 11.0 Complete Analysis
   engMountTWW, // Increases from TWW mount using old parts
   furnishingComponents, // Midnight Expanse Furnishing Components
-  dragonflight1, // Dragonflight Market
   tier10_1, // Tier 10.1 Complete Analysis
   consumablesTier10_1, // Consumables Tier 10.1 Complete Analysis
   tier10_2, // Tier 10.2 Complete Analysis
   consumablesTier10_2, // Consumables Tier 10.2 Complete Analysis
+  dragonflight1, // Dragonflight Market
 ]
 
 export default recommendedConfigs
