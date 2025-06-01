c00b438f18141f955e69aa27a3432dfd1efc85b5
cohenaj194 <cohenaj194@gmail.com>
2025-06-01 18:16:08 -0400

    addding all combos

--- app/components/recommended/WoW/WeeklyPriceGroup/index.ts
+++ app/components/recommended/WoW/WeeklyPriceGroup/index.ts
@@ -7,17 +7,21 @@ import consumablesTier11_1 from './consumablesTier11_1'
 import tier10_1 from './tier10_1'
 import tier10_2 from './tier10_2'
 import consumablesTier10_1 from './consumablesTier10_1'
+import consumablesTier11_0 from './consumablesTier11_0'
+import consumablesTier10_2 from './consumablesTier10_2'
 
 const recommendedConfigs = [
   tier11_1, // Tier 11.1 Complete Analysis
-  tier11_0, // Tier 11.0 Complete Analysis
   consumablesTier11_1, // Consumables Tier 11.1 Complete Analysis
+  tier11_0, // Tier 11.0 Complete Analysis
+  consumablesTier11_0, // Consumables Tier 11.0 Complete Analysis
   engMountTWW, // Increases from TWW mount using old parts
   furnishingComponents, // Midnight Expanse Furnishing Components
   dragonflight1, // Dragonflight Market
   tier10_1, // Tier 10.1 Complete Analysis
   consumablesTier10_1, // Consumables Tier 10.1 Complete Analysis
   tier10_2, // Tier 10.2 Complete Analysis
+  consumablesTier10_2, // Consumables Tier 10.2 Complete Analysis
 ]
 
 export default recommendedConfigs
--- app/components/recommended/WoW/WeeklyPriceGroup/tier11_0.ts
+++ app/components/recommended/WoW/WeeklyPriceGroup/tier11_0.ts
@@ -9,7 +9,7 @@ const tier11_0 = {
     start_day: 1,
     end_year: 2024,
     end_month: 12,
-    end_day: 1,
+    end_day: 30,
     price_groups: [
       {
         name: 'Jc',
