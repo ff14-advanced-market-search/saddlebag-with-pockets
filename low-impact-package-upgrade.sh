3fdabb01e5bcf1913aa843e2a47aa89f0fd50b49
cohenaj194 <cohenaj194@gmail.com>
2025-06-11 15:31:05 -0400

    adding minor bash fixes

--- low-impact-package-upgrade.sh
+++ low-impact-package-upgrade.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 # Minor/Patch updates (safer)
 yarn upgrade @datadog/browser-rum@latest
@@ -12,3 +14,5 @@ yarn upgrade highcharts-react-official@latest
 yarn upgrade react-dnd-scrolling@latest
 yarn upgrade zod@latest
 yarn upgrade wrangler@latest
+
+echo "All package upgrades completed successfully."
