3d346ce192fcb8d7f487acfedfe475944e10765e
cohenaj194 <cohenaj194@gmail.com>
2025-07-06 23:26:54 -0400

    dont show this in ci/cd

--- .cloudflare/build.sh
+++ .cloudflare/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Echo all env vars for debugging
-echo "DISCORD_CLIENT_ID: $DISCORD_CLIENT_ID"
-echo "DISCORD_CLIENT_SECRET: $DISCORD_CLIENT_SECRET"
-echo "NODE_VERSION: $NODE_VERSION"
-echo "SITE_NAME: $SITE_NAME"
+# # Echo all env vars for debugging
+# echo "DISCORD_CLIENT_ID: $DISCORD_CLIENT_ID"
+# echo "DISCORD_CLIENT_SECRET: $DISCORD_CLIENT_SECRET"
+# echo "NODE_VERSION: $NODE_VERSION"
+# echo "SITE_NAME: $SITE_NAME"
 
 # Write wrangler.toml dynamically
 cat <<EOF > wrangler.toml
