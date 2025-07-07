a1f3968d83c3324ab3bcf0227f031ba9c2c84c1a
cohenaj194 <cohenaj194@gmail.com>
2025-07-06 22:36:10 -0400

    see if its all vars or just secrets

--- .cloudflare/build.sh
+++ .cloudflare/build.sh
@@ -3,8 +3,8 @@
 # Echo all env vars for debugging (optional, remove if you don't want secrets in logs)
 echo "DISCORD_CLIENT_ID: $DISCORD_CLIENT_ID"
 echo "DISCORD_CLIENT_SECRET: $DISCORD_CLIENT_SECRET"
-# echo "NODE_VERSION: $NODE_VERSION"
-# echo "SITE_NAME: $SITE_NAME"
+echo "NODE_VERSION: $NODE_VERSION"
+echo "SITE_NAME: $SITE_NAME"
 
 # Write wrangler.toml dynamically
 cat <<EOF > wrangler.toml
