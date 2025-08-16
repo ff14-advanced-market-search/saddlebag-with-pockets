3064ecb9f12463575a9d1e2a262cf9a1b81d9c5b
Chris Rolfe <christopher.rolfe198@gmail.com>
2025-08-16 17:47:09 +0200

    various v2 upgrades

--- remix.config.js
+++ remix.config.js
@@ -1,16 +1,21 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: 'cloudflare-pages',
+  publicPath: '/build/',
+  serverBuildPath: 'functions/[[path]].js',
+  serverConditions: ['worker'],
+  serverMainFields: ['browser', 'module', 'main'],
+  serverModuleFormat: 'esm',
+  serverPlatform: 'neutral',
+  serverDependenciesToBundle: 'all',
+  serverMinify: true,
   server: './server.js',
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
   future: {
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
-    v2_errorBoundary: true
+    v2_errorBoundary: true,
+    v2_dev: true,
+    v2_headers: true
   }
 }
