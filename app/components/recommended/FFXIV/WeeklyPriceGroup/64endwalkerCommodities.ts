0f5c7227d43b3934fd34f6f77b4d086d08cc98b7
cohenaj194 <cohenaj194@gmail.com>
2025-08-23 18:24:54 -0400

    adding more price groups

---
+++ app/components/recommended/FFXIV/WeeklyPriceGroup/64endwalkerCommodities.ts
@@ -0,0 +1,239 @@
+const endwalker64Commodities = {
+  name: 'Endwalker 6.4 Commodities',
+  description:
+    'Monitor price trends for Endwalker 6.4 commodities across all categories.',
+  config: {
+    region: 'NA',
+    start_year: 2023,
+    start_month: 4,
+    start_day: 23,
+    end_year: 2023,
+    end_month: 9,
+    end_day: 3,
+    minimum_marketshare: 10000000,
+    price_setting: 'median',
+    quantity_setting: 'quantitySold',
+    price_groups: [
+      { name: 'Medicine NQ', item_ids: [], categories: [43], hq_only: false },
+      { name: 'Medicine HQ', item_ids: [], categories: [43], hq_only: true },
+
+      {
+        name: 'Ingredients NQ',
+        item_ids: [],
+        categories: [44],
+        hq_only: false
+      },
+      { name: 'Ingredients HQ', item_ids: [], categories: [44], hq_only: true },
+
+      { name: 'Meals NQ', item_ids: [], categories: [45], hq_only: false },
+      { name: 'Meals HQ', item_ids: [], categories: [45], hq_only: true },
+
+      { name: 'Seafood NQ', item_ids: [], categories: [46], hq_only: false },
+      { name: 'Seafood HQ', item_ids: [], categories: [46], hq_only: true },
+
+      { name: 'Stone NQ', item_ids: [], categories: [47], hq_only: false },
+      { name: 'Stone HQ', item_ids: [], categories: [47], hq_only: true },
+
+      { name: 'Metal NQ', item_ids: [], categories: [48], hq_only: false },
+      { name: 'Metal HQ', item_ids: [], categories: [48], hq_only: true },
+
+      { name: 'Lumber NQ', item_ids: [], categories: [49], hq_only: false },
+      { name: 'Lumber HQ', item_ids: [], categories: [49], hq_only: true },
+
+      { name: 'Cloth NQ', item_ids: [], categories: [50], hq_only: false },
+      { name: 'Cloth HQ', item_ids: [], categories: [50], hq_only: true },
+
+      { name: 'Leather NQ', item_ids: [], categories: [51], hq_only: false },
+      { name: 'Leather HQ', item_ids: [], categories: [51], hq_only: true },
+
+      { name: 'Bone NQ', item_ids: [], categories: [52], hq_only: false },
+      { name: 'Bone HQ', item_ids: [], categories: [52], hq_only: true },
+
+      { name: 'Reagents NQ', item_ids: [], categories: [53], hq_only: false },
+      { name: 'Reagents HQ', item_ids: [], categories: [53], hq_only: true },
+
+      { name: 'Dyes NQ', item_ids: [], categories: [54], hq_only: false },
+      { name: 'Dyes HQ', item_ids: [], categories: [54], hq_only: true },
+
+      {
+        name: 'Weapon Parts NQ',
+        item_ids: [],
+        categories: [55],
+        hq_only: false
+      },
+      {
+        name: 'Weapon Parts HQ',
+        item_ids: [],
+        categories: [55],
+        hq_only: true
+      },
+
+      {
+        name: 'Furnishings NQ',
+        item_ids: [],
+        categories: [56],
+        hq_only: false
+      },
+      { name: 'Furnishings HQ', item_ids: [], categories: [56], hq_only: true },
+
+      { name: 'Materia NQ', item_ids: [], categories: [57], hq_only: false },
+      { name: 'Materia HQ', item_ids: [], categories: [57], hq_only: true },
+
+      { name: 'Crystals NQ', item_ids: [], categories: [58], hq_only: false },
+      { name: 'Crystals HQ', item_ids: [], categories: [58], hq_only: true },
+
+      { name: 'Catalysts NQ', item_ids: [], categories: [59], hq_only: false },
+      { name: 'Catalysts HQ', item_ids: [], categories: [59], hq_only: true },
+
+      { name: 'Miscellany NQ', item_ids: [], categories: [60], hq_only: false },
+      { name: 'Miscellany HQ', item_ids: [], categories: [60], hq_only: true },
+
+      {
+        name: 'Exterior Fixtures NQ',
+        item_ids: [],
+        categories: [65],
+        hq_only: false
+      },
+      {
+        name: 'Exterior Fixtures HQ',
+        item_ids: [],
+        categories: [65],
+        hq_only: true
+      },
+
+      {
+        name: 'Interior Fixtures NQ',
+        item_ids: [],
+        categories: [66],
+        hq_only: false
+      },
+      {
+        name: 'Interior Fixtures HQ',
+        item_ids: [],
+        categories: [66],
+        hq_only: true
+      },
+
+      {
+        name: 'Outdoor Furnishings NQ',
+        item_ids: [],
+        categories: [67],
+        hq_only: false
+      },
+      {
+        name: 'Outdoor Furnishings HQ',
+        item_ids: [],
+        categories: [67],
+        hq_only: true
+      },
+
+      {
+        name: 'Chairs and Beds NQ',
+        item_ids: [],
+        categories: [68],
+        hq_only: false
+      },
+      {
+        name: 'Chairs and Beds HQ',
+        item_ids: [],
+        categories: [68],
+        hq_only: true
+      },
+
+      { name: 'Tables NQ', item_ids: [], categories: [69], hq_only: false },
+      { name: 'Tables HQ', item_ids: [], categories: [69], hq_only: true },
+
+      { name: 'Tabletop NQ', item_ids: [], categories: [70], hq_only: false },
+      { name: 'Tabletop HQ', item_ids: [], categories: [70], hq_only: true },
+
+      {
+        name: 'Wall-mounted NQ',
+        item_ids: [],
+        categories: [71],
+        hq_only: false
+      },
+      {
+        name: 'Wall-mounted HQ',
+        item_ids: [],
+        categories: [71],
+        hq_only: true
+      },
+
+      { name: 'Rugs NQ', item_ids: [], categories: [72], hq_only: false },
+      { name: 'Rugs HQ', item_ids: [], categories: [72], hq_only: true },
+
+      {
+        name: 'Seasonal Miscellany NQ',
+        item_ids: [],
+        categories: [74],
+        hq_only: false
+      },
+      {
+        name: 'Seasonal Miscellany HQ',
+        item_ids: [],
+        categories: [74],
+        hq_only: true
+      },
+
+      { name: 'Minions NQ', item_ids: [], categories: [75], hq_only: false },
+      { name: 'Minions HQ', item_ids: [], categories: [75], hq_only: true },
+
+      {
+        name: 'Airship Submersible Components NQ',
+        item_ids: [],
+        categories: [79],
+        hq_only: false
+      },
+      {
+        name: 'Airship Submersible Components HQ',
+        item_ids: [],
+        categories: [79],
+        hq_only: true
+      },
+
+      {
+        name: 'Orchestrion Components NQ',
+        item_ids: [],
+        categories: [80],
+        hq_only: false
+      },
+      {
+        name: 'Orchestrion Components HQ',
+        item_ids: [],
+        categories: [80],
+        hq_only: true
+      },
+
+      {
+        name: 'Gardening Items NQ',
+        item_ids: [],
+        categories: [81],
+        hq_only: false
+      },
+      {
+        name: 'Gardening Items HQ',
+        item_ids: [],
+        categories: [81],
+        hq_only: true
+      },
+
+      { name: 'Paintings NQ', item_ids: [], categories: [82], hq_only: false },
+      { name: 'Paintings HQ', item_ids: [], categories: [82], hq_only: true },
+
+      {
+        name: 'Registrable Miscellany NQ',
+        item_ids: [],
+        categories: [90],
+        hq_only: false
+      },
+      {
+        name: 'Registrable Miscellany HQ',
+        item_ids: [],
+        categories: [90],
+        hq_only: true
+      }
+    ]
+  }
+}
+
+export default endwalker64Commodities
--- app/components/recommended/FFXIV/WeeklyPriceGroup/index.ts
+++ app/components/recommended/FFXIV/WeeklyPriceGroup/index.ts
@@ -1,8 +1,24 @@
 import materia from './materia'
 import furnishings from './furnishings'
 import rawMats65 from './rawMats65'
-import rawMats72 from './rawMats72'
+import dawntrail70Commodities from './70dawntrailCommodities'
+import dawntrail71Commodities from './71dawntrailCommodities'
+import dawntrail72Commodities from './72dawntrailCommodities'
+import dawntrail73Commodities from './73dawntrailCommodities'
+import endwalker63Commodities from './63endwalkerCommodities'
+import endwalker64Commodities from './64endwalkerCommodities'
+import endwalker65Commodities from './65endwalkerCommodities'
 
-const recommendedConfigs = [materia, furnishings, rawMats65, rawMats72]
+const recommendedConfigs = [
+  materia,
+  furnishings,
+  dawntrail70Commodities,
+  dawntrail71Commodities,
+  dawntrail72Commodities,
+  dawntrail73Commodities,
+  endwalker63Commodities,
+  endwalker64Commodities,
+  endwalker65Commodities
+]
 
 export default recommendedConfigs
