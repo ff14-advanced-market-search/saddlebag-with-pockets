f7eacb659fd6e662943687c55639e144f30a5b94
cohenaj194 <cohenaj194@gmail.com>
2025-09-07 15:31:24 -0400

    split the big groups up

--- app/components/recommended/FFXIV/WeeklyPriceGroup/index.ts
+++ app/components/recommended/FFXIV/WeeklyPriceGroup/index.ts
@@ -5,6 +5,7 @@ import dawntrail70Commodities from './70dawntrailCommodities'
 import dawntrail71Commodities from './71dawntrailCommodities'
 import dawntrail72Commodities from './72dawntrailCommodities'
 import dawntrail73Commodities from './73dawntrailCommodities'
+import dawntrail73Misc from './73dawntrailMisc'
 import endwalker63Commodities from './63endwalkerCommodities'
 import endwalker64Commodities from './64endwalkerCommodities'
 import endwalker65Commodities from './65endwalkerCommodities'
@@ -13,6 +14,7 @@ const recommendedConfigs = [
   materia,
   furnishings,
   dawntrail73Commodities,
+  dawntrail73Misc,
   dawntrail72Commodities,
   dawntrail71Commodities,
   dawntrail70Commodities,
