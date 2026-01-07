920fd756beb72be61f11e101bec974b2361133ce
Alex Cohen <cohenaj194@gmail.com>
2026-01-06 20:54:50 -0500

    Gw2 weekly recommended (#688)
    * adding the weekly delta stuff for gw2
    
    * adding items by tier
    
    * adding misc
    
    * fix min prices
    
    * fix misc table
    
    * minor fixes

---
+++ app/components/recommended/GW2/WeeklyPriceGroup/index.ts
@@ -0,0 +1,11 @@
+import craftingItemsByTable from './craftingItemsByTable'
+import craftingItemsByTier from './craftingItemsByTier'
+import craftingItemsByMiscTier from './craftingItemsByMiscTier'
+
+const recommendedConfigs = [
+  craftingItemsByTable,
+  craftingItemsByTier,
+  craftingItemsByMiscTier
+]
+
+export default recommendedConfigs
