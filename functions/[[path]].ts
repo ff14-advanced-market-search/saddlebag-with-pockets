989a8f68473b5d0809b742bb8abec306df7b4d73
djdemers <davidjdemers@gmail.com>
2026-01-20 14:48:42 -0800

    fix: polyfill global before server build in functions entry using TLA

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,13 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
+
+// Polyfill `global` for Cloudflare Workers before loading the server bundle
+const globalRef = globalThis as typeof globalThis & { global?: typeof globalThis };
+if (!globalRef.global) {
+  globalRef.global = globalRef;
+}
+
 // @ts-expect-error - Vite generates this from the build output
-import * as build from "../build/server/index.js";
+const build = await import("../build/server/index.js");
 
 const handleRequest = createPagesFunctionHandler({
   build,
