bb13f203c809b96737d2d70d4e826a82bcb8aed0
Alex Cohen <cohenaj194@gmail.com>
2026-03-07 19:25:39 -0500

    adding for 12.0 (#710)
    * adding for 12.0
    
    * add import
    
    * fix titles

--- app/components/recommended/WoW/WeeklyPriceGroup/consumablesTier11_0.ts
+++ app/components/recommended/WoW/WeeklyPriceGroup/consumablesTier11_0.ts
@@ -1,5 +1,5 @@
 const consumablesTier11_0 = {
-  name: 'TWW Raid Consumables 11.0 Consumables',
+  name: 'TWW Raid 11.0 Consumables',
   description:
     'Comprehensive price tracking for all major TWW consumables for the first raid. Food, flasks, potions, gems, enchants, etc.',
   config: {
