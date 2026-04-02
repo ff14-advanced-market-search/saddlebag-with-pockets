ea12f75a3df6b4916bdc4e22d8cf82390e8c4f61
cohenaj194 <cohenaj194@gmail.com>
2026-04-02 17:55:58 -0400

    update user agent as weve had one all along

--- app/requests/client/config.ts
+++ app/requests/client/config.ts
@@ -8,5 +8,5 @@ export const address = defaultAddress
 //     ? process.env.API_ADDRESS
 //     : defaultAddress
 
-export const UserAgent = 'Saddlebag/1.0'
+export const UserAgent = 'Saddlebag-Frontend/1.0.0'
 export const defaultMaxAge = 60 * 60 * 24 * 365 // 1 year in seconds
